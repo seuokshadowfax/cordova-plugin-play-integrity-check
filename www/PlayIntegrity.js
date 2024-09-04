e3ae986b8f132e400be95475a6b24aa05bf1ef3f
Pavan Madusha <pavan.madhusha@directfn.com>
2024-09-04 16:26:26 +0530

    Update Play Intergrity Check

--- www/PlayIntegrity.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var PlayIntegrity = {
-    getIntegrityToken: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'PlayIntegrity', 'getIntegrityToken', []);
-    }
-};
-
-module.exports = PlayIntegrity;
---
+++ www/integrity.js
@@ -0,0 +1,7 @@
+/* global cordova, module */
+
+module.exports = {
+    check: function(nonce, successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Integrity", "check", [nonce]);
+    }
+};
