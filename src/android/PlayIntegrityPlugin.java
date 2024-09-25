cb6fd76336df96b7a4ae08b2ad6adb11d47c9a7a
Pavan Madusha <pavan.madhusha@directfn.com>
2024-09-25 15:33:18 +0530

    Add Nonce to Play Integrity Check

--- src/android/PlayIntegrityPlugin.java
+++ src/android/PlayIntegrityPlugin.java
@@ -1,4 +1,4 @@
-package com.android.playintegrity;
+package com.android.playintegrity.PlayIntegrityPlugin;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -8,20 +8,26 @@ import com.google.android.gms.tasks.Tasks;
 import com.google.android.play.core.integrity.IntegrityManager;
 import com.google.android.play.core.integrity.IntegrityManagerFactory;
 import com.google.android.play.core.integrity.IntegrityTokenResponse;
+import com.google.android.play.core.integrity.IntegrityTokenRequest;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import java.security.SecureRandom;
+import android.util.Base64;
 
 public class PlayIntegrityPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("getIntegrityToken")) {
+            String nonce = generateNonce();
             // Call Play Integrity API here
             IntegrityManager integrityManager = IntegrityManagerFactory.create(cordova.getActivity().getApplicationContext());
 
-            Task<IntegrityTokenResponse> integrityTokenResponse = integrityManager.requestIntegrityToken(/* Request Parameters */);
+            IntegrityTokenRequest request = IntegrityTokenRequest.builder().setNonce(nonce).build();
+
+            Task<IntegrityTokenResponse> integrityTokenResponse = integrityManager.requestIntegrityToken(request);
 
             integrityTokenResponse.addOnSuccessListener(new OnSuccessListener<IntegrityTokenResponse>() {
                 @Override
@@ -38,5 +44,13 @@ public class PlayIntegrityPlugin extends CordovaPlugin {
             return true;
         }
         return false;
+    }
+
+    private String generateNonce() {
+        byte[] nonceBytes = new byte[16]; // 16 bytes
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(nonceBytes);
+
+        return Base64.encodeToString(nonceBytes, Base64.URL_SAFE | Base64.NO_WRAP);
     }
 }
